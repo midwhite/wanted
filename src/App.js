89417d06afb71567499521bfe4b6538d10c59c12
Hiroki Takano <takano@hitokuse.com>
2017-08-01 22:21:34 +0900

    create Bingo function

--- src/App.js
+++ src/App.js
@@ -6,18 +6,39 @@ class App extends Component {
     super(props);
     this.state = {
       selectedCard: null,
+      cells: [],
       cards: [{
         name: "Hiroki",
+        contents: [
+          "勉強が好きすぎて大学に5年間通った。",
+          "彼女が欲しすぎて出会い系サイトを開発してしまった。",
+          "一部ではかいちょーと呼ばれている。"
+        ],
         tags: ["広告", "エンジニア", "経済"]
       },{
-        name: "Hiroki",
-        tags: ["広告", "エンジニア", "経済"]
+        name: "Hiroshi",
+        contents: [
+          "勉強が好きすぎて大学に5年間通った。",
+          "彼女が欲しすぎて出会い系サイトを開発してしまった。",
+          "一部ではかいちょーと呼ばれている。"
+        ],
+        tags: ["コンサル", "教育", "東大"]
       },{
-        name: "Hiroki",
-        tags: ["広告", "エンジニア", "経済"]
+        name: "Shiho",
+        contents: [
+          "勉強が好きすぎて大学に5年間通った。",
+          "彼女が欲しすぎて出会い系サイトを開発してしまった。",
+          "一部ではかいちょーと呼ばれている。"
+        ],
+        tags: ["大学院生", "社会学", "林業"]
       },{
-        name: "Hiroki",
-        tags: ["広告", "エンジニア", "経済"]
+        name: "にしやん",
+        contents: [
+          "勉強が好きすぎて大学に5年間通った。",
+          "彼女が欲しすぎて出会い系サイトを開発してしまった。",
+          "一部ではかいちょーと呼ばれている。"
+        ],
+        tags: ["読書会", "映画祭", "関西弁"]
       }]
     };
     this.tagColors = ["#2196F3", "#009688", "#FF5722"]
@@ -28,20 +49,41 @@ class App extends Component {
   closeSelectedCard(){
     this.setState({ selectedCard: null });
   }
+  touchCell(){
+    if(this.state.cells.indexOf(this.state.selectedCard) === -1){
+      const cells = this.state.cells;
+      cells.push(this.state.selectedCard);
+      this.setState({ selectedCard: null, cells: cells });
+    } else {
+      this.setState({ selectedCard: null });
+    }
+  }
+  resetCell(){
+    const cells = [];
+    this.state.cells.forEach(cellNum => {
+      if(cellNum !== this.state.selectedCard) cells.push(cellNum);
+    });
+    this.setState({ selectedCard: null, cells: cells });
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
-            <h1>Wanted!</h1>
+            <h1 style={{fontWeight: "bold", textAlign: "center"}}>Wanted!</h1>
           </div>
         </div>
         <div className="cards row">
         {this.state.cards.map((card, i) => {
+          const cardStyle = {};
+          if(this.state.cells.indexOf(i) !== -1){
+            cardStyle.background = "#424242";
+            cardStyle.color      = "#FFF";
+          }
           return(
-            <div key={`card${i}`} className={"card col-xs-4 col-sm-2"} onClick={this.selectCard.bind(this, i)}>
-              <h4>{card.name}</h4>
-              <div className="tags">
+            <div key={`card${i}`} className={"card col-xs-6 col-sm-2"} style={cardStyle} onClick={this.selectCard.bind(this, i)}>
+              <h4 style={{textAlign: "center", fontWeight: "bold"}}>{card.name}</h4>
+              <div className="tags" style={{paddingBottom: 15}}>
               {card.tags.map((tag, j)=>{
                 return <p key={`card${i}_tag${j}`} className="tag" style={{background: this.tagColors[j]}}>{card.tags[j]}</p>;
               })}
@@ -51,7 +93,7 @@ class App extends Component {
         })}
         </div>
         {(()=>{
-          if(this.state.selectedCard){
+          if(this.state.selectedCard != null){
             const card = this.state.cards[this.state.selectedCard];
             return(
               <div className="selectedCard">
@@ -69,11 +111,28 @@ class App extends Component {
                     </div>
                     <div className="row">
                       <div className="tags col-xs-12">
-                      {card.tags.map((tag, i)=>{
-                        return(
-                          <span key={`selectedCard_tag${i}`} className="tag" style={{background: this.tagColors[i]}}>{tag}</span>
-                        );
-                      })}
+                        {card.tags.map((tag, i)=>{ return <span key={`selectedCard_tag${i}`} className="tag" style={{background: this.tagColors[i]}}>{tag}</span>; })}
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="contents col-xs-12">
+                        {card.contents.map((content, i)=>{
+                          return(
+                            <div key={`selectedCard_content${i}`} className="content">{content}</div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-xs-12">
+                      {(()=>{
+                        if(this.state.cells.indexOf(this.state.selectedCard) !== -1){
+                          return <div className="btn btn-default form-control" style={{margin: "10px auto"}} onClick={this.resetCell.bind(this)}>会えてなかった！</div>;
+                        } else {
+                          return <div className="btn form-control" style={{margin: "10px auto", background: "#E91E63", color: "#FFF"}} onClick={this.touchCell.bind(this)}>会えた！</div>;
+                        }
+                      })()}
+
                       </div>
                     </div>
                   </div>
